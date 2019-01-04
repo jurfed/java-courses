08220c7a61ad2bf7e20354660c82696ea61b2a4a
jur <jurfed@inbox.ru>
2019-01-04 12:50:40 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,21 @@
 public class Calculate{
 	public static void main(String [] args){
-		System.out.println("Hello world");
+		System.out.println("Calculate...");
+		int first = Integer.valueOf(args[0]);
+		int second = Integer.valueOf(args[1]);
+		int sum = first+second;
+		System.out.println("sum " + sum);
+		
+		int minus = first - second;
+		System.out.println(minus);
+		
+		double div = (double)first/second;
+		System.out.println(div);
+		
+		double mult = first*second;
+		System.out.println(mult);
+		
+		double pow = Math.pow(first, second);
+		System.out.println(pow);
 	}
 }
